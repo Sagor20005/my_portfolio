e3f73a3d7eb3e2205ef41822a71289dc6393eddd
Md Jakareya <109013040+Sagor20005@users.noreply.github.com>
2025-02-10 20:39:37 +0600

    Add cafe website

--- js/data.js
+++ js/data.js
@@ -7,6 +7,15 @@ const projacts = [
     image:"https://ik.imagekit.io/1a42ie8sg/myProject/download.png?updatedAt=1737081459267",
     url:"https://jaraz.vercel.app"
   },
+  {
+    image:"https://ik.imagekit.io/1a42ie8sg/myProject/Restaurant.png?updatedAt=1739197882385",
+    name:"Cafe Shop",
+    tags:[ "html","css","scss","scroll reveal" ],
+    discription:"This is an Cafe website. this website fulfiled all requiremrnt of clint. castumars or visitors attruct this website",
+    type:"Shop",
+    url:"https://cafe-steel-two.vercel.app/"
+  },
+  
   ]
   
   
